497b33a6f4e24eba931d40bcfba380bc487d6cb9
cecen <191208060+DevCecen@users.noreply.github.com>
2026-04-02 10:48:20 +0300

    Python: Division

---
+++ python/python_division.py
@@ -0,0 +1,9 @@
+# Problem: https://www.hackerrank.com/challenges/python-division/problem
+# Difficulty: Easy
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+print(a//b)
+print(a/b)
