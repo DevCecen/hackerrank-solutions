b93a22153351c0f8edbf7da03ee554055fbb3315
cecen <191208060+DevCecen@users.noreply.github.com>
2026-04-03 11:12:49 +0300

    Loops

---
+++ python/loops.py
@@ -0,0 +1,7 @@
+Problem: https://www.hackerrank.com/challenges/python-loops/problem
+Difficulty: Easy
+
+if __name__ == '__main__':
+    n = int(input())
+    for x in range(n):
+        print(x**2)
