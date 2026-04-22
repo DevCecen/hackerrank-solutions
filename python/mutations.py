4a0f377b4ef5b99a543bc9b0d9557a0311ca6d27
cecen <191208060+DevCecen@users.noreply.github.com>
2026-04-22 19:06:50 +0300

    Mutations

---
+++ python/mutations.py
@@ -0,0 +1,15 @@
+#Problem : https://www.hackerrank.com/challenges/python-mutations/problem
+#Difficulty: Easy
+
+def mutate_string(string, position, character):
+    my_list = []
+    my_list = list(string)
+    my_list[position] = character
+    return "".join(my_list)
+    
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
