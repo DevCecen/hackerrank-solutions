3e6fb52339f99b2c7f3f35f5c8210a140af68f0a
cecen <191208060+DevCecen@users.noreply.github.com>
2026-08-27 17:42:38 +0300

    solve capitalize problem

---
+++ python/capitalize.py
@@ -0,0 +1,34 @@
+#problem: https://www.hackerrank.com/challenges/capitalize/problem
+#difficulty: Easy
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    s_list = s.split(" ")
+    result = ""
+    for i in range(len(s_list)):
+        if i != len(s_list) - 1:
+            result += (f"{str(s_list[i].capitalize())} ")
+        else:
+            result += (f"{str(s_list[i].capitalize())}")
+    return result
+        
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
